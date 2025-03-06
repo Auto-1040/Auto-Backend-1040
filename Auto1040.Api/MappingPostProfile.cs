54c03ee1b02a82ee2648d8ee3142feacdd1b35e2
Miriam-Frieder <m0556777068@gmail.com>
2025-03-06 22:19:52 +0200

    upload, user is done

---
+++ Auto1040.Api/MappingPostProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Leyadech.Api.PostModels;
+using Leyadech.Core.DTOs;
+using Leyadech.Core.Entities;
+
+namespace Leyadech.Api
+{
+    public class MappingPostProfile:Profile
+    {
+        public MappingPostProfile()
+        {
+            CreateMap<MotherPostModel, MotherDto>();
+        }
+    }
+}
---
+++ Auto1040.Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Leyadech.Core.Entities;
+
+namespace Auto1040.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> MotherData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
